7137afba4a4ce19ed8f1cfc54bfc8cf8e4f733db
rrep <pwduerr@gmail.com>
2020-01-29 10:06:26 -0700

    added fetch to alphavantage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import Header from './components/Header'
 import SymbolSearch from './components/SymbolSearch'
+import Quote from './components/Quote'
 import './App.css';
 
 function App() {
 
+  const [currentQuote, setCurrentQuote] = useState({});
+
+  const BASE_END_POINT = "https://www.alphavantage.co/query?function=";
+  const API_KEY = "&apikey=KV2W4LMZHNA2CXTH";
+  const FUNCTION_QUERY = "GLOBAL_QUOTE"
+  
+
+
   const searchClick = (searchString) =>{
-    console.log(searchString);
+    const symbol = "&symbol="+searchString;
+    const stockQuoteEndPoint = `${BASE_END_POINT}${FUNCTION_QUERY}${symbol}${API_KEY}`;
+    console.log(stockQuoteEndPoint);
+    fetch(stockQuoteEndPoint).then((response)=>{
+      return response.json()
+    }).then((currentStockTickerObject) => {
+      setCurrentQuote(currentStockTickerObject);
+       // Get current Quote.
+    }).catch((error)=>{
+      console.log(`The following errors have arisen: ${error}`);
+    });
   }
 
   return (
     <div className="App">
       <Header/>
-      <SymbolSearch searchClick={searchClick}/>    
+      <SymbolSearch searchClick={searchClick}/> 
+      <Quote currentQuote={currentQuote}/>   
     </div>
   );
 }
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Quote(props) {
+
+    return(
+        <div></div>
+
+)
+}
+
+export default Quote;
