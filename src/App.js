7993268e6ad7df3c09ea6b67043ac3a89c505570
rrep <pwduerr@gmail.com>
2020-01-31 17:10:36 -0700

    pass timeseries data to historygraph

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ function App() {
   const [currentQuote, setCurrentQuote] = useState({});
   const [currentSearch, setCurrentSearch] = useState([]);
   const [newsItems, setNewsItems] = useState([]);
+  const [timeSeries, setTimeSeries] = useState([]);
   
 
   const BASE_ALPHAVANTAGE_END_POINT = "https://www.alphavantage.co/query?function=";
   const ALPHAV_API_KEY = "&apikey=KV2W4LMZHNA2CXTH";
   const ALPHAV_FUNCTION_QUERY = "GLOBAL_QUOTE";
   const ALPHAV_SEARCH_QUERY = "SYMBOL_SEARCH";
+  const ALPHAV_TIME_SERIES_QUERY = "TIME_SERIES_DAILY";
   
   const BASE_NEWSAPI_END_POINT = "https://newsapi.org/v2/everything?q="; //add symbol from search
   const BASE_NEWSAPI_KEY="from=2020-01-29&sortBy=popularity&apiKey=17b781dab8984272be12d4b8b3f6442d";
 
-  
 
   const searchClick = (searchString) =>{
     if (searchString === ''){
@@ -34,18 +35,24 @@ function App() {
       const keywords = "&keywords="+searchString;
       const stockQuoteEndPoint = `${BASE_ALPHAVANTAGE_END_POINT}${ALPHAV_FUNCTION_QUERY}${symbol}${ALPHAV_API_KEY}`;
       const stockSearchEndPoint= `${BASE_ALPHAVANTAGE_END_POINT}${ALPHAV_SEARCH_QUERY}${keywords}${ALPHAV_API_KEY}`;
-      console.log(stockQuoteEndPoint);
+      const timeSeriesEndPoint = `${BASE_ALPHAVANTAGE_END_POINT}${ALPHAV_TIME_SERIES_QUERY}${symbol}${ALPHAV_API_KEY}`;
+      console.log(timeSeriesEndPoint);
+      
       //fetch symbol quote
       fetch(stockQuoteEndPoint).then((response)=>{
         return response.json()
       }).then((currentStockTickerObject) => {
         setCurrentQuote(currentStockTickerObject["Global Quote"]);
-        console.log(currentStockTickerObject);
          // Search for name
       }).then(fetch(stockSearchEndPoint).then((response)=>{
         return response.json()
       }).then((currentStockSearchObject) => {
         setCurrentSearch(currentStockSearchObject.bestMatches[0]['2. name']);
+        //get time series
+      })).then(fetch(timeSeriesEndPoint).then((response)=>{
+        return response.json()
+      }).then((currentTimeSeriesObject) => {
+        setTimeSeries(currentTimeSeriesObject['Time Series (Daily)']);
       })).catch((error)=>{
         console.log(`The following errors have arisen: ${error}`);
       });
@@ -69,7 +76,7 @@ function App() {
           <br/><br/>
           <Quote currentQuote={currentQuote}/>
           <br/>
-          <HistoryGraph/>        
+          {Object.entries(timeSeries).length > 0? <HistoryGraph timeSeries={timeSeries}></HistoryGraph>:null}
         </div>
       </section>
     </div>
--- src/components/HistoryGraph.js
+++ src/components/HistoryGraph.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 function HistoryGraph(props) {
+
+    //take in data object and process it to make an array for the line chart
     function parseData(data){
         const dataArray = [];
         for (var i in data){
+            const obj = data[i];
             dataArray.push({
-                date: new Date(Object.keys(data)[0].replace(/-/g, '/')),
-                value: Object.keys(data)[0]
-
+                date: new Date(i.replace(/-/g, '/')),
+                value: obj["4. close"]
             });
         }
-
         return dataArray;
     }
     return(
         <div>
+            {console.log(parseData(props.timeSeries))}
             <svg className="line-chart"></svg>
         </div>
     )
